2be962cedefab3e774d0075a21306a04250c3423
Marcin Wilk <marcin.wilk.503@gmail.com>
2025-02-11 20:17:39 +0900

    event listeners added to search engine results, search button fetch function implemented

--- src/main.js
+++ src/main.js
@@ -31,14 +31,6 @@ locationInput.addEventListener("input", (event) => {
 
 });
 
-locationInput.addEventListener("blur", () => {
-  console.log("unfocused");
-  if(resultsDiv) {
-    resultsDiv.remove();
-  }
-
-});
-
 locationInput.addEventListener("focus", () => {
   handleLastResult(lastResult);
 });
@@ -53,37 +45,84 @@ function handleLastResult(lastResult) {
 
 
 
+// search engine implementation
+let resultsDivChildren;
 function updateSearch(userInput) {
   clearResults();
   const results = fuse.search(userInput, {limit: 5});
 
+  results.forEach((element, index) => {
+    const div = document.createElement("div");
+    div.innerHTML = `<p id="result-${index}" class="hover:cursor-pointer">${element.item.capital}, ${element.item.country}</p>`;
+    resultsDiv.appendChild(div);
+  });
 
+  resultsDivChildren = resultsDiv.children;
 
-  resultsDiv.innerHTML = results
-  .map((item, index) => `<div id="${index}"><p>${item.item.capital}, ${item.item.country}</p><div>`)
-  .join("");
+  console.log(resultsDivChildren);
+
   suggestions.className = "flex flex-col relative";
-  resultsDiv.className = "absolute w-[250px] flex max-w-[250px] bg-zinc-300";
+  resultsDiv.className = "absolute w-[250px] flex flex-col max-w-[250px] bg-zinc-300";
   suggestions.appendChild(resultsDiv);
   lastResult = resultsDiv.innerHTML;
 };
 
 
+//handles removing search results after clicking on one of the pTags and sets value of input
+locationInput.addEventListener("blur", () => {
+  console.log("unfocused");
+  if(resultsDiv) {
+    for(let i = 0; i < resultsDivChildren.length; i++) {
+      const pTag = document.getElementById(`result-${i}`);
+      pTag.addEventListener("click", () => {
+        // make id=input-location the value of selected pTag
+        locationInput.value = `${pTag.innerText}`;
+        resultsDiv.remove();
+      });
+    };
+  };
+});
+
+
 function clearResults() {
   if(suggestions){
-    suggestions.innerHTML = ``;
-    suggestions.className = ``;
+    resultsDiv.innerHTML = ``;
+    resultsDiv.className = ``;
   };
   return;
 };
 
 
 
-function handleSearch() {
+searchBtn.addEventListener("click", () => {
+  handleSearchBtn(locationInput.value);
+});
 
-  fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${worldCapitals[0].capital}?key=${CONFIG.API_KEY}`)
+async function handleSearchBtn(locationInput) {
+  const queryData = locationInput.split(",")[0].trim();
+  const queryResult = await handleSearch(queryData);
+  const result = [queryResult.currentConditions.datetime, queryResult.currentConditions.temp];
+  console.log(queryResult.currentConditions.datetime);
+  
+
+
+
+  const temperatureDiv = document.createElement("div");
+  temperatureDiv.className = "flex absolute w-[350px] top-100 bg-red-200 justify-center"
+  temperatureDiv.innerHTML = `Time:${result[0]}, Temperature: ${result[1]} Farenheit`;
+  searchDiv.prepend(temperatureDiv);
+}
+
+
+
+function handleSearch(queryData) {
+
+  return fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${queryData}?key=${CONFIG.API_KEY}`)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    console.log(data)
+    return data;
+  })
   .catch(error => console.log(error));  
 
 }
